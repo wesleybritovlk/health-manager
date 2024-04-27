94415ba37fbb72f104d0bdb821d1fc98bcf5d87c
Wesley Brito <wesleymuniz20@gmail.com>
2024-04-27 19:01:24 -0300

    feat: add base spring project

---
+++ dev/backend/src/main/java/com/github/wesleybritovlk/healthmanager/HealthmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.wesleybritovlk.healthmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthmanagerApplication.class, args);
+	}
+
+}
