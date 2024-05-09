0d447b6bbf0d552ddd6d338a5e1907da178d445a
Wesley Brito <wesleymuniz20@gmail.com>
2024-05-09 03:41:18 -0300

    test: test base get from api controller

---
+++ dev/backend/src/test/java/com/github/wesleybritovlk/healthmanager/app/ApiControllerTest.java
@@ -0,0 +1,26 @@
+package com.github.wesleybritovlk.healthmanager.app;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ApiController.class)
+class ApiControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void itShouldReturnHomeApiResponse() throws Exception {
+        mockMvc.perform(get("/api")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Health Manager API"))
+                .andExpect(jsonPath("$.version").value("0.0.1-SNAPSHOT"))
+                .andDo(print());
+    }
+}
